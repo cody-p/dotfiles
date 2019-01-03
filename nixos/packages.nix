9b5434c71e06f6c3600b0487be602175df6bb552
cody-p <professional@voidlurker.net>
2019-01-03 06:30:01 -0800

    changed config

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -31,5 +31,6 @@
         cowsay
         fortune
         gksu
+        arandr
     ];
 }
--- nixos/services.nix
+++ nixos/services.nix
@@ -34,6 +34,13 @@
                 #sessionCommands = ''
                 #    xrandr --output HDMI-1-3 --off --output DVI-D-0 --off --output HDMI-1-2 --off --output HDMI-1-1 --off --output DP-1-2 --mode 1600x900 --pos 3840x72 --rotate normal --output DP-1-1 --off --output                     HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-5 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off                 --output DP-0 --off
                 #'';
+                gdm = {
+                    wayland = false;
+                    enable = true;
+                };
+                setupCommands = ''
+                    xrandr --output DVI-D-0 --mode 1600x900 --pos 3840x88 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-5 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
+                '';
             };
         };
     };
